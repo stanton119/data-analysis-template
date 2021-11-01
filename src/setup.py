606fd2bd87a17fd756113b5d472dd173de5155f5
Richard Stanton <stanton1191@gmail.com>
2021-11-01 21:28:51 +0000

    Moving version number to init.py only

--- src/setup.py
+++ src/setup.py
@@ -7,9 +7,12 @@ with open("requirements.txt", "r", encoding="utf-8") as f:
         if req and not req.startswith("--"):
             requires.append(req)
 
+with open("project/__init__.py") as f:
+    version = f.readlines()[-1].split()[-1].strip("\"'")
+
 setup(
     name="project",
-    version="0.1",
+    version=version,
     packages=find_packages(exclude=["tests"]),
     install_requires=requires,
 )
